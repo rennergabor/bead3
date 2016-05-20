0bb61bc62813ea5e13c5358dacb80113c32997c9
Gabor Renner <rg@intrend.hu>
2016-05-21 01:44:48 +0200

    jm

--- jatekmester.hpp
+++ jatekmester.hpp
@@ -2,19 +2,32 @@
 #define JATEKMESTER_HPP_INCLUDED
 
 #include <string>
+#include <vector>
 
 using namespace std;
 
 class Mezo {
-public:
     int _player, _i, _j;
+public:
     Mezo(int p, int i, int j): _player(p), _i(i), _j(j){}
+};
+
+enum Irany {ED, KNY, EK, ENY};  // Észak-Dél, Kelet-Nyugat, Észak-Kelet, Észak-nyugat
+
+class Vonal {
+    Irany _irany;
+    vector<Mezo*> mezok;
+public:
+    Vonal(Mezo *m1, Mezo* m2);  // ha nem szomszedosak, akkor ....
+    bool addMezo(Mezo *m);      // false, ha nem esik egybe a jelenlegiekkel
 };
 
 enum LepesEredmeny {LEPES_OK=1, HIBAS_LEPES, NYERT, DONTETLEN};
 
 class JatekMester {
+//    private <vector<>>[2] =
 public:
     LepesEredmeny lepes(Mezo *mezore);
 };
+
 #endif // JATEKMESTER_HPP_INCLUDED
